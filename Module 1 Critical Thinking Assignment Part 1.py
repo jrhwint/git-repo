40a4c2421dfbe9d283d6b3f81b0b0ae0eb8da472
jrhwint <157253274+jrhwint@users.noreply.github.com>
2024-01-20 16:08:22 -0700

    Add files via upload

---
+++ Module 1 Critical Thinking Assignment Part 1.py
@@ -0,0 +1,12 @@
+# Module 1 Critical Thinking Assignment
+
+#Part 1: Find the addition and subtraction of two numbers
+
+# Ask the user to input two numbers (num1 and num2)
+
+# add them together to find the output 
+
+# subtract the two numbers to find the output
+
+# 
+
